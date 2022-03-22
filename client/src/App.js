72f252484d63155a0c918e7b93631afec9524c4b
hyukzz <ri_chard@naver.com>
2022-03-22 23:54:42 +0900

    feat: 로그인 모달 끄기

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,39 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Signup from "./Components/Signup/Signup";
-import Intro from "./Components/intropage/Intro";
-import { createStore } from "redux";
-import { Provider, useSelector, useDispatch, connect, shallowEqual } from "react-redux";
+import logo from './logo.svg'
+import './App.css'
+import Signup from './Components/Signup/Signup'
+import Intro from './Components/intropage/Intro'
+import { createStore } from 'redux'
+import {
+	Provider,
+	useSelector,
+	useDispatch,
+	connect,
+	shallowEqual,
+} from 'react-redux'
+import Loginbtn from '../src/Components/Login/Loginbtn'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function reducer(currentState, action) {
-  if (currentState === undefined) {
-    return {};
-  }
+	if (currentState === undefined) {
+		return {}
+	}
 
-  const newState = { ...currentState };
-  return newState;
+	const newState = { ...currentState }
+	return newState
 }
-const store = createStore(reducer);
+const store = createStore(reducer)
 
 function App() {
-  return (
-    <div className="App">
-      <Signup />
-    </div>
-  );
+	return (
+		<div className="App">
+			{/* <Signup /> */}
+			<Router>
+				<Route path="/">
+					<Loginbtn />
+				</Route>
+			</Router>
+		</div>
+	)
 }
 
-export default App;
+export default App
---
+++ client/src/Components/Login/Loginoauth.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import styled from 'styled-components'
+import kakao_oauth from './kakao_oauth.png'
+
+const OauthContainer = styled.div`
+	width: 100%;
+	height: 7rem;
+	margin-top: 0.5rem;
+	display: flex;
+	flex-direction: column;
+	font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+	/* border: 1px solid blue; */
+	div {
+		width: 100%;
+		height: 3rem;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-size: 1rem;
+		color: white;
+		/* border: 1px solid white; */
+	}
+`
+const IconBox = styled.span`
+	width: 100%;
+	height: 80%;
+	/* margin-top: 1rem; */
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	/* border: 1px solid red; */
+	img {
+		width: 3.5rem;
+		height: 3.5rem;
+		margin: 0 0.5rem 0 0.5rem;
+		/* border: 1px solid white; */
+	}
+`
+
+function Loginoauth() {
+	// const REST_API_KEY = process.env.REACT_APP_REST_API_KEY
+	// const REDIRECT_URI = 'https://localhost:3000/oauth/kakao/callback'
+	// const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`
+
+	return (
+		<OauthContainer>
+			<IconBox>
+				{/* <a href={KAKAO_AUTH_URL}> */}
+				<a id="kakao" href="/auth/kakao">
+					<img src={kakao_oauth} />
+				</a>
+			</IconBox>
+		</OauthContainer>
+	)
+}
+export default Loginoauth
