6336a4e74e143373d0d96a2b73ed6b2ee226efe9
rkdghwnd <bsc6448@naver.com>
2022-03-24 21:56:37 +0900

    .

--- server/controllers/users/signup.js
+++ server/controllers/users/signup.js
@@ -1,18 +1,17 @@
 const { user } = require("../../models");
 require("dotenv").config();
 
-<<<<<<< HEAD
 module.exports = async (req, res) => {
   const userInfo = await user.findOne({
     where: {
-      userid: req.body.userid,
+      user_id: req.body.userid,
     },
   });
 
   if (!userInfo) {
     user
       .create({
-        userid: req.body.userid,
+        user_id: req.body.userid,
         password: req.body.password,
         nickname: req.body.nickname,
         email: req.body.email,
@@ -26,14 +25,4 @@ module.exports = async (req, res) => {
   } else {
     res.status(226).send({ message: "exist" });
   }
-=======
-module.exports = (req, res) => {
-  let payload = req.body
-
-  user.create(payload).then(
-  res.status(200).json({
-      message : "signup success!",
-      data: {payload}
-  }))
->>>>>>> 48d18e7bab098a22c58c40cb90d4896565a49cae
 };
--- server/index.js
+++ server/index.js
@@ -28,53 +28,36 @@ app.get("/users/logout", controllers.logout); //로그아웃
 //신규 유저 가입, 탈퇴, 유저 정보 조회, 유저 정보 변경
 app.post("/users", controllers.signup); // 유저 정보 변경
 app.delete("/users", controllers.signout); // 회원 탈퇴
-app.get("/users", controllers.userinfo);  // 유저 정보 조회
+app.get("/users", controllers.userinfo); // 유저 정보 조회
 app.patch("/users", controllers.changeinfo); // 유저 정보 변경
 
 //음식점 정보 조회, 사진업로드, 사진 업로드, 사진 삭제
 app.get("/shops/:shop_id", controllers.shopinfo); // 음식점 정보 조회
-app.post("/shops-pics", controllers.shoppic.post); // 
-app.delete("/shops-pics",controllers.shoppic.delete)
-app.get("/shops-tags",controllers.shoptag.get);
-app.post("/shops-tags",controllers.shoptag.post);
-app.delete("/shops-tags",controllers.shoptag.delete);
-app.patch("/shops-tags",controllers.shoptag.patch);
+app.post("/shops-pics", controllers.shoppic.post); //
+app.delete("/shops-pics", controllers.shoppic.delete);
+app.get("/shops-tags", controllers.shoptag.get);
+app.post("/shops-tags", controllers.shoptag.post);
+app.delete("/shops-tags", controllers.shoptag.delete);
+app.patch("/shops-tags", controllers.shoptag.patch);
 
-<<<<<<< HEAD
-//아티클 조회, 게시, 삭제, 수정
-app.post("/article", controllers.createarticle);
-app.delete("/article", controllers.delarticle);
-app.get("/article", controllers.patcharticle);
-app.get("/article", controllers.article);
-=======
 //아티클 조회, 업로드, 삭제, 수정
 app.post("/articles", controllers.createarticle);
-app.delete("/articles",controllers.delarticle);
+app.delete("/articles", controllers.delarticle);
 app.patch("/articles", controllers.patcharticle);
-app.get("/articles/:article_id", controllers.article)
-
->>>>>>> 48d18e7bab098a22c58c40cb90d4896565a49cae
+app.get("/articles/:article_id", controllers.article);
 
 //리뷰 조회, 업로드, 삭제, 수정, 리뷰사진 업로드, 리뷰사진 삭제
 app.post("/reviews", controllers.createreview);
-app.delete("/reviews",controllers.delreview);
+app.delete("/reviews", controllers.delreview);
 app.patch("/reviews", controllers.patchreview);
-app.get("/reviews", controllers.review)
+app.get("/reviews", controllers.review);
 app.post("/reviews-pics", controllers.reviewpic.post);
-app.delete("/reviews-pics",controllers.reviewpic.delete)
+app.delete("/reviews-pics", controllers.reviewpic.delete);
 
 //tag 조회, 업로드, 삭제
 app.post("/tags", controllers.tag.post);
 app.delete("/tags", controllers.tag.delete);
 app.patch("/tags", controllers.tag.patch);
-
-<<<<<<< HEAD
-app.post("/review", controllers.createreview);
-app.delete("/review", controllers.delreview);
-app.get("/review", controllers.patchreview);
-app.get("/review", controllers.review);
-=======
->>>>>>> 48d18e7bab098a22c58c40cb90d4896565a49cae
 
 const HTTPS_PORT = process.env.HTTPS_PORT || 4000;
 
@@ -86,7 +69,7 @@ if (fs.existsSync("./key.pem") && fs.existsSync("./cert.pem")) {
   const privateKey = fs.readFileSync(__dirname + "/key.pem", "utf8");
   const certificate = fs.readFileSync(__dirname + "/cert.pem", "utf8");
   const credentials = { key: privateKey, cert: certificate };
-
+  db.sequelize.sync();
   server = https.createServer(credentials, app);
   server.listen(HTTPS_PORT, () => console.log(`server runnning ${HTTPS_PORT}`));
 } else {
@@ -94,3 +77,23 @@ if (fs.existsSync("./key.pem") && fs.existsSync("./cert.pem")) {
 }
 
 module.exports = server;
+
+// let server;
+// if (fs.existsSync("./key.pem") && fs.existsSync("./cert.pem")) {
+//   const privateKey = fs.readFileSync(__dirname + "/key.pem", "utf8");
+//   const certificate = fs.readFileSync(__dirname + "/cert.pem", "utf8");
+//   const credentials = { key: privateKey, cert: certificate };
+
+//   server = https.createServer(credentials, app);
+//   db.sequelize.sync().then((req) => {
+//     app.listen(HTTPS_PORT, function () {
+//       console.log(`listen on ${HTTPS_PORT}`);
+//     });
+//   });
+// } else {
+//   db.sequelize.sync().then((req) => {
+//     app.listen(HTTPS_PORT, function () {
+//       console.log(`listen on ${HTTPS_PORT}`);
+//     });
+//   });
+// }
