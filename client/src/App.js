e01758ac0db8bbdc1b92ffc104bca0557c2c1fbd
rkdghwnd <bsc6448@naver.com>
2022-03-31 20:14:08 +0900

    에러수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,132 +1,132 @@
-import logo from './logo.svg'
-import './App.css'
-import { createStore } from 'redux'
+import logo from "./logo.svg";
+import "./App.css";
+import { createStore } from "redux";
 import {
-	Provider,
-	useSelector,
-	useDispatch,
-	connect,
-	shallowEqual,
-} from 'react-redux'
-import SlideShop from './Components/Mainpage/SlideShop'
-import SlidePick from './Components/Mainpage/SlidePick'
-import Header from './Components/Mainpage/Header'
-import Footer from './Components/Mainpage/Footer'
-import './App.css'
-import Signup from './Components/Signup/Signup'
-import Intro from './Components/intropage/Intro'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import SlideTopic from './Components/Mainpage/SlideTopic'
-import Hashtag from './Components/Mainpage/Hashtag'
-import Loginmodal from './Components/Login/Loginmodal'
-import Mypage from './Components/Mypage/Mypage'
-import Signout from './Components/Mypage/Signout/Signout'
-import Review from './Components/Mypage/Review'
-import Favorite from './Components/Favorites/Favorites'
-import ShopDetail from './Components/ShopDetail/ShopDetail'
-import ShopDetail2 from './Components/ShopDetail/ShopDetail2'
-import EditorPick from './Components/EditorPick/EditorPick'
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+  Provider,
+  useSelector,
+  useDispatch,
+  connect,
+  shallowEqual,
+} from "react-redux";
+import SlideShop from "./Components/Mainpage/SlideShop";
+import SlidePick from "./Components/Mainpage/SlidePick";
+import Header from "./Components/Mainpage/Header";
+import Footer from "./Components/Mainpage/Footer";
+import "./App.css";
+import Signup from "./Components/Signup/Signup";
+import Intro from "./Components/intropage/Intro";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import SlideTopic from "./Components/Mainpage/SlideTopic";
+import Hashtag from "./Components/Mainpage/Hashtag";
+import Loginmodal from "./Components/Login/Loginmodal";
+import Mypage from "./Components/Mypage/Mypage";
+import Signout from "./Components/Mypage/Signout/Signout";
+import Review from "./Components/Review/Review";
+import Favorite from "./Components/Favorites/Favorites";
+import ShopDetail from "./Components/ShopDetail/ShopDetail";
+import ShopDetail2 from "./Components/ShopDetail/ShopDetail2";
+import EditorPick from "./Components/EditorPick/EditorPick";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-import dummyTopicShopInfo from './dummy/dummyTopicShopInfo'
-import Myreview from './Components/Mypage/Myreview/Myreview'
+import dummyTopicShopInfo from "./dummy/dummyTopicShopInfo";
+import Myreview from "./Components/Mypage/Myreview/Myreview";
 
 function App() {
-	document.body.style.overflow = 'hidden'
-	const isLogInOpen = useSelector(state => state.isLogInOpen)
-	const isSignUpOpen = useSelector(state => state.isSignUpOpen)
-	const isFavoriteModal = useSelector(state => state.isFavoriteModal)
-	const shopInfo = useSelector(state => state.shopInfo)
-	const shopDetailInfo = useSelector(state => state.shopDetailInfo)
-	const dispatch = useDispatch()
+  document.body.style.overflow = "hidden";
+  const isLogInOpen = useSelector((state) => state.isLogInOpen);
+  const isSignUpOpen = useSelector((state) => state.isSignUpOpen);
+  const isFavoriteModal = useSelector((state) => state.isFavoriteModal);
+  const shopInfo = useSelector((state) => state.shopInfo);
+  const shopDetailInfo = useSelector((state) => state.shopDetailInfo);
+  const dispatch = useDispatch();
 
-	const [topicInfo, setTopicInfo] = useState(dummyTopicShopInfo)
+  const [topicInfo, setTopicInfo] = useState(dummyTopicShopInfo);
 
-	useEffect(() => {
-		function getLocation() {
-			if (navigator.geolocation) {
-				// GPS를 지원하면
-				navigator.geolocation.getCurrentPosition(
-					function (position) {},
-					function (error) {
-						console.error(error)
-					},
-					{
-						enableHighAccuracy: false,
-						maximumAge: 0,
-						timeout: Infinity,
-					}
-				)
-			} else {
-				alert('GPS를 지원하지 않습니다')
-			}
-		}
-		getLocation()
-	}, [])
+  useEffect(() => {
+    function getLocation() {
+      if (navigator.geolocation) {
+        // GPS를 지원하면
+        navigator.geolocation.getCurrentPosition(
+          function (position) {},
+          function (error) {
+            console.error(error);
+          },
+          {
+            enableHighAccuracy: false,
+            maximumAge: 0,
+            timeout: Infinity,
+          }
+        );
+      } else {
+        alert("GPS를 지원하지 않습니다");
+      }
+    }
+    getLocation();
+  }, []);
 
-	// useEffect(() => {
-	// 	axios
-	// 		.get('https://localhost:4000/topicshop/total_review', {
-	// 			withCredentials: true,
-	// 		})
-	// 		.then(res => {
-	// 			dispatch({
-	// 				type: 'topic_shop_info',
-	// 				data: res.data.data.shopInfo,
-	// 			})
-	// 			dispatch({
-	// 				type: 'topic_shop_detail_info',
-	// 				data: res.data.data.shopPicInfos,
-	// 			})
-	// 			setTopicInfo(res.data.data.shopInfo)
-	// 		})
-	// }, [])
+  // useEffect(() => {
+  // 	axios
+  // 		.get('https://localhost:4000/topicshop/total_review', {
+  // 			withCredentials: true,
+  // 		})
+  // 		.then(res => {
+  // 			dispatch({
+  // 				type: 'topic_shop_info',
+  // 				data: res.data.data.shopInfo,
+  // 			})
+  // 			dispatch({
+  // 				type: 'topic_shop_detail_info',
+  // 				data: res.data.data.shopPicInfos,
+  // 			})
+  // 			setTopicInfo(res.data.data.shopInfo)
+  // 		})
+  // }, [])
 
-	if (localStorage.getItem('visited') === null) {
-		localStorage.setItem('visited', JSON.stringify([]))
-	}
+  if (localStorage.getItem("visited") === null) {
+    localStorage.setItem("visited", JSON.stringify([]));
+  }
 
-	// console.log("카카오로 받아온 정보", shopInfo);
-	// console.log("크롤링으로 받아온 정보", shopDetailInfo);
+  // console.log("카카오로 받아온 정보", shopInfo);
+  // console.log("크롤링으로 받아온 정보", shopDetailInfo);
 
-	return (
-		<div className="App">
-			<BrowserRouter>
-				<Header />
-				{isLogInOpen ? (
-					<Loginmodal />
-				) : (
-					(document.body.style.overflow = 'unset')
-				)}
-				{isSignUpOpen ? <Signup /> : (document.body.style.overflow = 'unset')}
-				{isFavoriteModal ? (
-					<Favorite />
-				) : (
-					(document.body.style.overflow = 'unset')
-				)}
-				<Switch>
-					<Route exact path="/">
-						<Intro />
-						<SlideShop />
-						<SlidePick />
-						<SlideTopic />
-					</Route>
-					<Route path="/mypage">
-						<Mypage />
-					</Route>
-					<Route path="/signout">
-						<Signout />
-					</Route>
-					<Route path="/shopdetail/:id" exact component={ShopDetail} />
-					<Route path="/shopdetail2/:id" component={ShopDetail2} />
-					<Route path="/review" component={Review} />
-					<Route path="/editor_pick/:code" component={EditorPick} />
-				</Switch>
-				<Footer />
-			</BrowserRouter>
-			{/* <EditorPick /> */}
-		</div>
-	)
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Header />
+        {isLogInOpen ? (
+          <Loginmodal />
+        ) : (
+          (document.body.style.overflow = "unset")
+        )}
+        {isSignUpOpen ? <Signup /> : (document.body.style.overflow = "unset")}
+        {isFavoriteModal ? (
+          <Favorite />
+        ) : (
+          (document.body.style.overflow = "unset")
+        )}
+        <Switch>
+          <Route exact path="/">
+            <Intro />
+            <SlideShop />
+            <SlidePick />
+            <SlideTopic />
+          </Route>
+          <Route path="/mypage">
+            <Mypage />
+          </Route>
+          <Route path="/signout">
+            <Signout />
+          </Route>
+          <Route path="/shopdetail/:id" exact component={ShopDetail} />
+          <Route path="/shopdetail2/:id" component={ShopDetail2} />
+          <Route path="/review" component={Review} />
+          <Route path="/editor_pick/:code" component={EditorPick} />
+        </Switch>
+        <Footer />
+      </BrowserRouter>
+      {/* <EditorPick /> */}
+    </div>
+  );
 }
-export default App
+export default App;
--- client/src/Components/ShopDetail/ShopDetail2.js
+++ client/src/Components/ShopDetail/ShopDetail2.js
@@ -1,9 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { memo, useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { BsStar } from "react-icons/bs";
-import Review from "../Review/Review";
+import { BiMessageDetail } from "react-icons/bi";
 import ShopImageModal from "./ShopImageModal";
+import ReviewPhotoModal from "./ReviewPhotoModal";
+import Loader from "./Loader";
 import {
   ShopImages,
   ShopBody,
@@ -11,29 +13,36 @@ import {
   ShopBasicInfoHeader,
   ShopDetailInfo,
   ShopReview,
+  ShopEachReview,
+  ShopReviewPlusButton,
   ShopLocation,
   Buttons,
   ReviewButton,
-  ReviewIcon,
-  FavoriteButton,
 } from "./ShopDetail.style";
-import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
 /*global kakao*/
 
-export default function ShopDetail2({ match }) {
-  const [isOpen, setOpen] = useState(false);
+export default function ShopDetail({ match }) {
+  const [isOpen, setOpen] = useState(false); // 음식점 이미지 Modal
+  const [isReviewOpen, setReviewOpen] = useState(false); // 리뷰 업로드 사진 Modal
   const [currentImage, setCurrentImage] = useState("");
+  const [currentReview, setCurrentReview] = useState(0);
   const [info, setInfo] = useState({ shop_pics: [], menus: [], reviews: [] });
-  let reviewCount = 5;
 
-  const shopInfo = useSelector((state) => state.shopInfo);
-  // const shopDetailInfo = useSelector((state) => state.shopDetailInfo);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [reviewCount, setReviewCount] = useState(4);
 
-  // console.log(shopDetailInfo[match.params.id]);
-  const handleImageClick = (item, i) => {
+  const dispatch = useDispatch();
+
+  const handleImageClick = (idx) => {
     setOpen(true);
-    setCurrentImage(i);
+    setCurrentImage(idx);
+  };
+
+  const handleReviewClick = (idx) => {
+    setReviewOpen(true);
+    setCurrentReview(idx);
   };
 
   const handleStar = () => {
@@ -42,24 +51,23 @@ export default function ShopDetail2({ match }) {
     console.log("hello");
   };
 
+  const handleReviewPlus = async () => {
+    setIsLoaded(true);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    setReviewCount(reviewCount + 4);
+    setIsLoaded(false);
+  };
+
   useEffect(() => {
-    // 카카오 map 객체 생성
-    var container = document.getElementById("map");
-    var options = {
-      center: new kakao.maps.LatLng(37.63462628004234, 126.83257800122841),
-      level: 3,
-    };
-    var map = new kakao.maps.Map(container, options);
+    console.log(reviewCount);
+  }, [reviewCount]);
 
-    // TODO: 메인페이지에서 shopid를 내려주면 엔드포인트에 붙여서 get요청
-
+  useEffect(() => {
+    // match.params.id 활용
     axios
       .get(`https://localhost:4000/shops2/${match.params.id}`)
       .then((res) => {
-        setInfo(res.data.data.targetshop);
-        console.log(res.data.data.targetshop);
-
-        // 방문한페이지 추가하는 로직 추가
+        // 방문한페이지 추가하는 로직 추가----------------------------
         if (
           window.location.href.includes("https://localhost:3000/shopdetail")
         ) {
@@ -84,6 +92,53 @@ export default function ShopDetail2({ match }) {
             console.log(JSON.stringify(visited));
           }
         }
+        // -------------------------------------------------
+        setInfo(res.data.data.targetshop);
+        console.log(res.data.data.targetshop);
+        dispatch({
+          type: "current_shop_id",
+          payload: {
+            shopId: res.data.data.targetshop.id,
+          },
+        });
+        return res.data.data.targetshop;
+      })
+      // KAKAO map api and marker
+      .then((res) => {
+        var container = document.getElementById("map");
+        var options = {
+          center: new kakao.maps.LatLng(res.y, res.x),
+          level: 3,
+        };
+        var map = new kakao.maps.Map(container, options);
+
+        // 마커가 표시될 위치입니다
+        var markerPosition = new kakao.maps.LatLng(res.y, res.x);
+
+        // 마커를 생성합니다
+        var marker = new kakao.maps.Marker({
+          position: markerPosition,
+          clickable: true, // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
+        });
+
+        // 마커가 지도 위에 표시되도록 설정합니다
+        marker.setMap(map);
+
+        // 마커를 클릭했을 때 마커 위에 표시할 인포윈도우를 생성합니다
+        var iwContent = `<div style="padding:5px;">${res.shop_name}</div>`, // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+          iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
+
+        // 인포윈도우를 생성합니다
+        var infowindow = new kakao.maps.InfoWindow({
+          content: iwContent,
+          removable: iwRemoveable,
+        });
+
+        // 마커에 클릭이벤트를 등록합니다
+        kakao.maps.event.addListener(marker, "click", function () {
+          // 마커 위에 인포윈도우를 표시합니다
+          infowindow.open(map, marker);
+        });
       });
   }, []);
 
@@ -98,13 +153,21 @@ export default function ShopDetail2({ match }) {
       ) : (
         <></>
       )}
+      {isReviewOpen ? (
+        <ReviewPhotoModal
+          setReviewOpen={setReviewOpen}
+          imageSet={info.reviews[currentReview].review_pics}
+        />
+      ) : (
+        <></>
+      )}
       <ShopImages>
-        {info.shop_pics.map((item, i) => {
+        {info.shop_pics.map((item, idx) => {
           return (
             <img
-              key={i}
+              key={idx}
               src={item.pic_URL}
-              onClick={() => handleImageClick(item, i)}
+              onClick={() => handleImageClick(idx)}
             />
           );
         })}
@@ -117,7 +180,7 @@ export default function ShopDetail2({ match }) {
             <Buttons>
               <Link to="/review">
                 <ReviewButton>
-                  <ReviewIcon />
+                  <BiMessageDetail className="reviewButton" />
                   <span>리뷰</span>
                 </ReviewButton>
               </Link>
@@ -144,24 +207,32 @@ export default function ShopDetail2({ match }) {
                   );
                 })}
               </li>
-              <li>{info.location}</li>
-              {info.menus.map((menu) => {
-                return <li>{`${menu.menu_name} : ${menu.price}`}</li>;
-              })}
             </ul>
           </ShopDetailInfo>
 
           <ShopReview>
-            {info.reviews.map((item, idx) => {
-              return (
-                <div key={idx} style={{ height: 30 }}>
-                  아이디 : {item.user_id}
-                  코멘트 : {item.comment}
-                  평점 : {item.star}
-                </div>
-              );
-            })}{" "}
-            {/* 빈 배열인 경우 아무 표시도 안됨 분기 나누어서 '리뷰 없음' 표시 해야 할듯*/}
+            {info.reviews.length !== 0
+              ? info.reviews.slice(0, reviewCount).map((item, idx) => {
+                  return (
+                    <ShopEachReview
+                      key={idx}
+                      style={{ height: 30 }}
+                      onClick={() => handleReviewClick(idx)}
+                    >
+                      아이디 : {item.user_id}
+                      코멘트 : {item.comment}
+                      평점 : {item.star}
+                    </ShopEachReview>
+                  );
+                })
+              : "리뷰 없음"}
+            {isLoaded ? (
+              <Loader />
+            ) : (
+              <ShopReviewPlusButton onClick={handleReviewPlus}>
+                더 보기
+              </ShopReviewPlusButton>
+            )}
           </ShopReview>
         </ShopBasicInfo>
         <ShopLocation id="map"></ShopLocation>
