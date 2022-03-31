32ac9e12fbd9b89614e4465afc9f138887ea3ce5
Richard <81045794+hyukzz@users.noreply.github.com>
2022-03-31 19:21:12 +0900

    Update Myreviewdelete.js

--- client/src/Components/Mypage/Myreview/Myreviewdelete.js
+++ client/src/Components/Mypage/Myreview/Myreviewdelete.js
@@ -1,8 +1,37 @@
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import React from 'react'
 
 const Myreviewdelete = () => {
 	const accessToken = localStorage.getItem('accessToken')
+
+	const [changeInfo, setchangeInfo] = useState({
+		review_id: '',
+	})
+	const userInfoHandler = () => {
+		if (!accessToken) {
+			return
+		} else {
+			axios
+				.get('https://localhost:4000/users', {
+					headers: { authorization: `Bearer ${accessToken}` },
+					'Content-Type': 'application/json',
+				})
+				.then(res => {
+					setchangeInfo(res.data.data.userInfo.reviews[0].id)
+					console.log(
+						'개인정보가져오기 성공',
+						res.data.data.userInfo.reviews[0]
+					)
+				})
+				.catch(err => {
+					console.log('개인가져오기 에러', err)
+				})
+		}
+	}
+	useEffect(() => {
+		userInfoHandler()
+	}, [])
+
 	const deleteModal = () => {
 		if (window.confirm('정말 리뷰를 삭제하시겠습니까?')) {
 			deleteHandler()
@@ -11,14 +40,22 @@ const Myreviewdelete = () => {
 		}
 	}
 	const deleteHandler = () => {
+		const { review_id } = changeInfo
+
 		if (!accessToken) {
 			return
 		} else {
 			axios
-				.delete('https://localhost:4000/reviews', {
-					headers: { authorization: `Bearer ${accessToken}` },
-					'Content-Type': 'application/json',
-				})
+				.delete(
+					'https://localhost:4000/reviews',
+					{
+						review_id,
+					},
+					{
+						headers: { authorization: `Bearer ${accessToken}` },
+						'Content-Type': 'application/json',
+					}
+				)
 				.then(res => {
 					console.log('리뷰삭제')
 					alert('리뷰가 삭제됐습니다.')
@@ -29,7 +66,6 @@ const Myreviewdelete = () => {
 				})
 		}
 	}
-
 	return (
 		<div className="deleteBtn">
 			<button onClick={deleteModal}>x</button>
