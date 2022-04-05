484f92c1172a8f9b5aa693d2cc3b952dba995e8c
hyukzz <ri_chard@naver.com>
2022-04-05 20:24:07 +0900

    Design: Signout css

--- client/src/App.js
+++ client/src/App.js
@@ -1,78 +1,79 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { createStore } from "redux";
+import logo from './logo.svg'
+import './App.css'
+import { createStore } from 'redux'
 import {
-  Provider,
-  useSelector,
-  useDispatch,
-  connect,
-  shallowEqual,
-} from "react-redux";
-import SlideShop from "./Components/Mainpage/SlideShop";
-import SlidePick from "./Components/Mainpage/SlidePick";
-import Header from "./Components/Mainpage/Header";
-import Footer from "./Components/Mainpage/Footer";
-import "./App.css";
-import Signup from "./Components/Signup/Signup";
-import Intro from "./Components/intropage/Intro";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import SlideTopic from "./Components/Mainpage/SlideTopic";
-import Hashtag from "./Components/Mainpage/Hashtag";
-import Loginmodal from "./Components/Login/Loginmodal";
-import Mypage from "./Components/Mypage/Mypage";
-import Signout from "./Components/Mypage/Signout/Signout";
-import Review from "./Components/Review/Review";
-import Favorite from "./Components/Favorites/Favorites";
-import ShopDetail from "./Components/ShopDetail/ShopDetail";
-import EditorPick from "./Components/EditorPick/EditorPick";
-import OauthLoding from "./Components/Login/OauthLoading";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import Myreview from "./Components/Mypage/Myreview/Myreview";
-import { Redirect } from "react-router-dom";
+	Provider,
+	useSelector,
+	useDispatch,
+	connect,
+	shallowEqual,
+} from 'react-redux'
+import SlideShop from './Components/Mainpage/SlideShop'
+import SlidePick from './Components/Mainpage/SlidePick'
+import Header from './Components/Mainpage/Header'
+import Footer from './Components/Mainpage/Footer'
+import './App.css'
+import Signup from './Components/Signup/Signup'
+import Intro from './Components/intropage/Intro'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import SlideTopic from './Components/Mainpage/SlideTopic'
+import Hashtag from './Components/Mainpage/Hashtag'
+import Loginmodal from './Components/Login/Loginmodal'
+import Mypage from './Components/Mypage/Mypage'
+import Signout from './Components/Mypage/Signout/Signout'
+import Review from './Components/Review/Review'
+import Favorite from './Components/Favorites/Favorites'
+import ShopDetail from './Components/ShopDetail/ShopDetail'
+import EditorPick from './Components/EditorPick/EditorPick'
+import OauthLoding from './Components/Login/OauthLoading'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Redirect } from 'react-router-dom'
 
 function App() {
-  const isLogInOpen = useSelector((state) => state.isLogInOpen);
-  const isSignUpOpen = useSelector((state) => state.isSignUpOpen);
-  const isFavoriteModal = useSelector((state) => state.isFavoriteModal);
+	const isLogInOpen = useSelector(state => state.isLogInOpen)
+	const isSignUpOpen = useSelector(state => state.isSignUpOpen)
+	const isFavoriteModal = useSelector(state => state.isFavoriteModal)
 
-  if (localStorage.getItem("visited") === null) {
-    localStorage.setItem("visited", JSON.stringify([]));
-  }
+	if (localStorage.getItem('visited') === null) {
+		localStorage.setItem('visited', JSON.stringify([]))
+	}
 
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Header />
-        {isLogInOpen ? <Loginmodal /> : ""}
-        {isSignUpOpen ? <Signup /> : ""}
-        {isFavoriteModal ? <Favorite /> : ""}
-        <Switch>
-          <Route exact path="/">
-            <Intro />
-            <SlideShop />
-            <SlidePick />
-            <SlideTopic />
-          </Route>
-          <Route path="/mypage">
-            <Mypage />
-          </Route>
-          <Route path="/signout">
-            <Signout />
-          </Route>
-          <Route path="/shopdetail/:id" component={ShopDetail} />
-          <Route path="/editor_pick/:code" component={EditorPick} />
-          {localStorage.getItem("accessToken") ? (
-            <Route path={"/review/:shop_id"} component={Review} />
-          ) : (
-            <Redirect to="/" />
-          )}
-          <Route path={"/review/:shop_id"} component={Review} />
-          <Route path="/oauthloding" component={OauthLoding} />
-        </Switch>
-        <Footer />
-      </BrowserRouter>
-    </div>
-  );
+	return (
+		<>
+			<div className="App">
+				<BrowserRouter>
+					<Header />
+					{isLogInOpen ? <Loginmodal /> : ''}
+					{isSignUpOpen ? <Signup /> : ''}
+					{isFavoriteModal ? <Favorite /> : ''}
+					<Switch>
+						<Route exact path="/">
+							<Intro />
+							<SlideShop />
+							<SlidePick />
+							<SlideTopic />
+						</Route>
+						<Route path="/mypage">
+							<Mypage />
+						</Route>
+						<Route path="/signout">
+							<Signout />
+						</Route>
+						<Route path="/shopdetail/:id" component={ShopDetail} />
+						<Route path="/editor_pick/:code" component={EditorPick} />
+						{localStorage.getItem('accessToken') ? (
+							<Route path={'/review/:shop_id'} component={Review} />
+						) : (
+							<Redirect to="/" />
+						)}
+						<Route path={'/review/:shop_id'} component={Review} />
+						<Route path="/oauthloding" component={OauthLoding} />
+					</Switch>
+					<Footer />
+				</BrowserRouter>
+			</div>
+		</>
+	)
 }
-export default App;
+export default App
