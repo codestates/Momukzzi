cf4c0bf82d93ebba645212fbd557f7c3a3148fbd
EuilimChoi <91925895+EuilimChoi@users.noreply.github.com>
2022-04-06 22:33:37 +0900

    Update userinfo.js

--- server/controllers/users/userinfo.js
+++ server/controllers/users/userinfo.js
@@ -46,36 +46,3 @@ module.exports = async (req, res) => {
 		res.status(400).json({ message: 'login first!' })
 	}
 }
-
-    if (logininfo) {
-        //요청 들러온 토큰 정리
-        const token = logininfo.split(" ")[1];
-        const data = jwt.verify(token, "1234", async (err, data) => {
-            if (err) {
-                res.status(400).json({ data: null, message: "token err" });
-            } else if (data) {
-                const userInfo = await user.findOne({
-                    where: {
-                        user_id: data.user_id,
-                    },
-                    include :[{
-                        //all: true, nested: true
-                        model :review,
-                        attributes : ['shop_id','star','comment','createdAt']
-                    },
-                    // {
-                    //     model : shop,
-                    //     attributes : ['shop_name']
-                    // }
-                    ]
-                });
-
-                res.status(200).json({ 
-                    message: "get userinfo!",
-                    data: { userInfo }});
-            }
-        });
-    } else {
-        res.status(400).json({ message: "login first!" });
-    }
-};
