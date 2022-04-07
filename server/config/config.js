bbe9d299a57ee562caecf55fbd0755edaec06eab
Ubuntu <ubuntu@ip-172-31-28-56.ec2.internal>
2022-04-07 16:56:17 +0000

    아마존 크롤링 되는 버전(아마도)

--- server/config/config.js
+++ server/config/config.js
@@ -3,24 +3,24 @@ dotenv.config();
 
 module.exports = {
   development: {
-    username: "root",
+    username: "admin",
     password: process.env.DATABASE_PASSWORD,
     database: "momukzzi",
-    host: "127.0.0.1",
+    host: "database-3.cpvcd8yfubpy.us-east-1.rds.amazonaws.com",
     dialect: "mysql",
   },
   test: {
-    username: "root",
+    username: "admin",
     password: process.env.DATABASE_PASSWORD,
     database: "momukzzi",
-    host: "127.0.0.1",
+    host: "database-3.cpvcd8yfubpy.us-east-1.rds.amazonaws.com",
     dialect: "mysql",
   },
   production: {
-    username: "root",
+    username: "admin",
     password: process.env.DATABASE_PASSWORD,
     database: "momukzzi",
-    host: "127.0.0.1",
+    host: "database-3.cpvcd8yfubpy.us-east-1.rds.amazonaws.com",
     dialect: "mysql",
   },
 };
