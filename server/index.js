da280e7bd1a715b7b202c0dd7ebb533f5b41ad93
EuilimChoi <91925895+EuilimChoi@users.noreply.github.com>
2022-04-09 23:43:52 +0900

    Update index.js

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: ["https://localhost:3000"],
+    origin: ["https://momukzzi.tk"],
     credentials: true,
     methods: ["GET", "POST", "OPTIONS", "PATCH", "DELETE"],
   })
