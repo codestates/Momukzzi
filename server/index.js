513f26b935c3406ca53d31aed5d7ca61273a64f1
EuilimChoi <91925895+EuilimChoi@users.noreply.github.com>
2022-04-12 18:26:50 +0900

    최종 버전 수정

--- server/index.js
+++ server/index.js
@@ -100,6 +100,7 @@ app.get("/topicshop/:topic", controllers.topicshop);
 
 // review 테이블 정보 여러개 가져오기
 app.post("/shopmanyreviews", controllers.shopmanyreviews);
+
 // 즐겨찾기 추가/제거
 app.post("/bookmark", controllers.bookmark);
 
