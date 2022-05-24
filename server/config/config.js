269824a6386a26199ad14473d9231ab019548b93
rkdghwnd <bsc6448@naver.com>
2022-05-24 18:29:41 +0900

    update

--- server/config/config.js
+++ server/config/config.js
@@ -3,24 +3,24 @@ dotenv.config();
 
 module.exports = {
   development: {
-    username: "admin",
-    password: process.env.DATABASE_PASSWORD,
+    username: "root",
+    password: "1234",
     database: "momukzzi",
-    host: "database-3.cpvcd8yfubpy.us-east-1.rds.amazonaws.com",
+    host: "127.0.0.1",
     dialect: "mysql",
   },
   test: {
-    username: "admin",
-    password: process.env.DATABASE_PASSWORD,
+    username: "root",
+    password: "1234",
     database: "momukzzi",
-    host: "database-3.cpvcd8yfubpy.us-east-1.rds.amazonaws.com",
+    host: "127.0.0.1",
     dialect: "mysql",
   },
   production: {
-    username: "admin",
-    password: process.env.DATABASE_PASSWORD,
+    username: "root",
+    password: "1234",
     database: "momukzzi",
-    host: "database-3.cpvcd8yfubpy.us-east-1.rds.amazonaws.com",
+    host: "127.0.0.1",
     dialect: "mysql",
   },
 };
